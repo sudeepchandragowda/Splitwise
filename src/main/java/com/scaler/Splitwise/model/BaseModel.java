09c7963efe2830c336f502a0a168e759dded2a77
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-07-13 06:27:20 +0530

    Updated application.properties, added BaseModel to model

---
+++ src/main/java/com/scaler/Splitwise/model/BaseModel.java
@@ -0,0 +1,26 @@
+package com.scaler.Splitwise.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @CreatedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+}
