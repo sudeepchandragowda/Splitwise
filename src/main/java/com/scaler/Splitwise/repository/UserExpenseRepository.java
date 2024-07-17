ed83d9b74e98efe4fdd8d6eff606d13c82ae3ffc
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-07-17 23:56:24 +0530

    Added remaining repositories

--- src/main/java/com/scaler/Splitwise/repository/UserExpenseRepository.java
+++ src/main/java/com/scaler/Splitwise/repository/UserExpenseRepository.java
@@ -2,8 +2,6 @@ package com.scaler.Splitwise.repository;
 
 import com.scaler.Splitwise.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository <Users, Long> {
+public interface UserExpenseRepository extends JpaRepository<Users, Long> {
 }
