6d92641260e9a95e79712db9307ba493b18e5129
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-07-20 23:52:27 +0530

    Updated annotations

--- src/main/java/com/scaler/Splitwise/model/Users.java
+++ src/main/java/com/scaler/Splitwise/model/Users.java
@@ -3,12 +3,15 @@ package com.scaler.Splitwise.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Scanner;
 
 @Data
 @Entity(name = "SPLITWISE_USER")
+@Builder
 public class Users extends BaseModel {
     private String name;
     private String email;
--- src/main/java/com/scaler/Splitwise/service/InitServiceImpl.java
+++ src/main/java/com/scaler/Splitwise/service/InitServiceImpl.java
@@ -1,8 +1,24 @@
 package com.scaler.Splitwise.service;
 
+import com.scaler.Splitwise.model.Users;
+import com.scaler.Splitwise.repository.ExpenseRepository;
+import com.scaler.Splitwise.repository.GroupRepository;
+import com.scaler.Splitwise.repository.UserExpenseRepository;
+import com.scaler.Splitwise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class InitServiceImpl implements InitService{
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ExpenseRepository expenseRepository;
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private UserExpenseRepository userExpenseRepository;
     @Override
     public void initialise() {
-
+        Users omkar = new Users();
+        omkar.setName("Omkar Akkirampalli");
     }
 }
